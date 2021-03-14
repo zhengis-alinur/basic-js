a5a0aa9b72602d7f2a72e6bd2869a69ef2932acd
zhengis-alinur <zhengis.alinur.erzhan@gmail.com>
2021-03-15 00:01:11 +0600

    feat: all tasks solved

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,20 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-Array.prototype.max = function() {
-  return Math.max.apply(null, this);
-};
-
 module.exports = class DepthCalculator {
-  tempParent = new Node(null);
-  calculateDepth(arr) {
+  calculateDepth(arr, parent) {
     // let node = new Node(null);
     // makeTree(arr, node);
     // return node.depth - 1;
+    if(typeof parent === "undefined") {
+      parent = new Node(null)
+    }
     for(let i = 0; i < arr.length; i++) {
       if(Array.isArray(arr[i])){
         let current = new Node(parent);
-        this.tempParent.childs.push(current);
-        makeTree(arr[i], current);
+        parent.childs.push(current);
+        this.calculateDepth(arr[i], current);
       }
     }
     let max = 1;
@@ -24,24 +22,8 @@ module.exports = class DepthCalculator {
       }
     }
     parent.depth = parent.depth + max;
+    return parent.depth - 1;
   }
-}
-
-function makeTree(arr, parent) {
-  for(let i = 0; i < arr.length; i++) {
-    if(Array.isArray(arr[i])){
-      current = new Node(parent);
-      parent.childs.push(current);
-      makeTree(arr[i], current);
-    }
-  }
-  let max = 1;
-  for(let i = 0; i < parent.childs.length; i++) {
-    if(max < parent.childs[i].depth) {
-      max = parent.childs[i].depth;
-    }
-  }
-  parent.depth = parent.depth + max;
 }
 
 function Node(parent) {
